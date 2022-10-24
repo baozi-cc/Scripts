534bbc608a24e34863aabe4e0532cfb973786cfa
CreamK <51256470+CreamK@users.noreply.github.com>
2022-10-24 16:47:28 +0800

    Update uploadGladosCookie.js

--- OtherScripts/uploadGladosCookie.js
+++ OtherScripts/uploadGladosCookie.js
@@ -25,7 +25,7 @@ function getCookie() {
       const gladosCookie = $request.headers["Cookie"];
       let currentCount = $.getdata('currentGladosCount');
       $.log(gladosCookie);
-      $.setdata(gladosCookie${currentCount}, gladosCookie);
+      $.setdata(gladosCookie,gladosCookie${currentCount});
       $.msg("GLaDOS", "", "获取签到Cookie成功🎉\ngladosCookie");
     }
 }
